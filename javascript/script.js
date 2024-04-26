ecd027cdd31cc16723521695b70d7f306c72558c
Crisstuff <148343622+Crisstuff@users.noreply.github.com>
2024-04-26 10:19:35 +0200

    dfhbhj

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,10 @@
+//første fuction er å lukke siden 
 function closeWindow() {
     window.close(); // This will attempt to close the current window/tab
 }
+var hvisPC{
+    
+}
 
 function newWindow(){
-    
 }
