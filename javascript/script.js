2427290a2370b42c8e3183390eb59c290b112d99
Crisstuff <148343622+Crisstuff@users.noreply.github.com>
2024-04-26 11:20:41 +0200

    nvfdjkn

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,33 @@
 function closeWindow() {
     window.close(); // This will attempt to close the current window/tab
 }
-var hvisPC{
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// gi verdi til ting for en indikator 
+
+//ncsdjkns
+var dataMaskin = document.getElementById("taDatamaskin")
+var deathToHerjus =document.getElementById("")
+
+
+//scroll funsjon 
+function scrollToSection(sectionId) {
+    let section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+}
+
+
+function tilKjøkken(){
     
 }
 
-function newWindow(){
+function tilbakeTilsoveRoom(){
+    if(dataMaskin = true) {
+        dataMaskin.scrollToSection(deathToHerjus);
+    }else {
+        dataMaskin.scrollToSection(tilbakeTilsoverRom-2);
+    }
 }
