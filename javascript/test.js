988a1e97c0a17b8431f62040de3fe7c0fcd2d3b2
Crisstuff <148343622+Crisstuff@users.noreply.github.com>
2024-04-28 12:17:03 +0200

    mflkdsj

--- javascript/test.js
+++ javascript/test.js
@@ -1,27 +1,51 @@
 let previousSections = [];
 
-        function scrollToSection(sectionId) {
+function scrollToSection(sectionId) {
+    let section = document.getElementById(sectionId);
+    if (section) {
+        // Lagre ID-en til den forrige seksjonen
+        previousSections.push(sectionId);
+        // Vis mål-avsnittet
+        section.style.display = 'flex';
+        // Rull til mål-avsnittet
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+}
+
+// ingenting kommer til å skje hvis du fjerner dette 
+// notat til DEGSELV kjekk hva denne gjør 
+// Legg til en lytter for å vise tidligere besøkte seksjoner når du skroller oppover
+window.addEventListener('scroll', () => {
+    if (previousSections.length > 0) {
+        previousSections.forEach(sectionId => {
             let section = document.getElementById(sectionId);
             if (section) {
-                // Lagre ID-en til den forrige seksjonen
-                previousSections.push(sectionId);
-
-                // Vis mål-avsnittet
                 section.style.display = 'flex';
-
-                // Rull til mål-avsnittet
-                section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-            }
-        }
-
-        // Legg til en lytter for å vise tidligere besøkte seksjoner når du skroller oppover
-        window.addEventListener('scroll', () => {
-            if (previousSections.length > 0) {
-                previousSections.forEach(sectionId => {
-                    let section = document.getElementById(sectionId);
-                    if (section) {
-                        section.style.display = 'flex';
-                    }
-                });
             }
         });
+    }
+});
+
+
+//tester scrolling via html dokumenter
+function scrollToStoryLine(storyLineHref) {
+    // Endre URL-en til politistoryline.html
+    const list = document.getElementsByTagName("p")
+
+    let section = window.getElementsByName(storyLineHref);
+    window.location.href ("politistoryline.html");
+    if (section) {
+        // Lagre ID-en til den forrige seksjonen
+        previousSections.push(storyLineHref || sectionId);
+        // Vis mål-avsnittet
+        section.style.display = 'flex';
+        // Rull til mål-avsnittet
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+}
+
+
+function myFunction() {
+    // Endre URL-en til politistoryline.html
+    window.location.href = "politistoryline.html";
+}
