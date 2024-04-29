f61a3f2bd67f46c8e3fb4778a4322a54c08247f0
Crisstuff <148343622+Crisstuff@users.noreply.github.com>
2024-04-29 14:10:56 +0200

    nsdckjn

---
+++ story.js
@@ -0,0 +1,46 @@
+let previousSections = [];
+
+function scrollToSection(sectionId) {
+    let section = document.getElementById(sectionId);
+    if (section) {
+        // Lagre ID-en til den forrige seksjonen
+        previousSections.push(sectionId);
+        // Vis mål-avsnittet
+        section.style.display = 'flex';
+        // Rull til mål-avsnittet
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' }) 
+    }
+}
+
+
+
+
+
+// Funksjon for å legge til en historie-seksjon til siden
+function addStorySection(chapterFile, sectionId) {
+    fetch(`sections/${chapterFile}.html`) // Hent riktig kapiteldokument
+        .then(response => response.text())
+        .then(html => {
+            const parser = new DOMParser(); //brukes til å parse (lese å forstå) data fra en html fil
+            const doc = parser.parseFromString(html, 'text/html');
+            const section = doc.getElementById(sectionId);
+            section.style.display = "block"; // Sørger for at seksjonen er synlig
+            const storyContainer = document.getElementById('story-container'); //finner kontainern vi vil putte innholdet i
+            storyContainer.innerHTML += section.outerHTML; // Legger til den valgte seksjonen til kontaineren
+            setupButtons(); // Setter opp knappene for ny lastet seksjon
+            sectionId.scrollIntoView({ behavior: 'smooth', block: 'start' }) 
+        }).catch(error => console.error('Error loading the section:', error));
+}
+
+// Funksjon for å sette opp event listeners for alle knapper
+function setupButtons() {
+    const buttons = document.querySelectorAll('.story-button'); //finner alle knappene som hører til historie-elementet
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            addStorySection(this.dataset.chapter, this.dataset.section);
+        });
+    });
+}
+
+// Initialiserer første del av historien
+addStorySection('start-1', 'start'); // Startseksjon fra kapittel 1
